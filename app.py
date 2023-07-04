6ffc619381977cdf249669e81cc0b97ef237d52a
advjosh <26547329+AdvJosh@users.noreply.github.com>
2023-07-03 21:01:24 -0500

    Setting things up

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 """
 My personal website built in Python with Flask. You already know that
 I like to keep my stuff public in the hopes that someone can learn from it :)
+
+Want to know more about me or get into contact with me? 
+Visit jmuth.com
 """
